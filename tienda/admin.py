61e6e32206e9f1f1f00c2fdf9cead50ae0e6e301
Lianet Tamarit Tejas <tamarittejas@gmail.com>
2026-05-03 21:52:57 -0400

    Fix database query in admin

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import Producto,Configuracion
+from .models import Producto, Configuracion
 
 admin.site.register(Producto)
-
-config = Configuracion.objects.first()
-if not config:
-    config = Configuracion.objects.create(
-        nombre_tienda="Mi Tienda",
-        whatsapp="5490000000000"
-    )
+admin.site.register(Configuracion) # Regístralo normal para poder editarlo
--- tienda/apps.py
+++ tienda/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_config(sender, **kwargs):
+    # Esto solo se ejecutará DESPUÉS de que las migraciones hayan terminado
+    from .models import Configuracion
+    if not Configuracion.objects.exists():
+        Configuracion.objects.create(
+            nombre_tienda="Mi Tienda",
+            whatsapp="5490000000000"
+        )
 
 class TiendaConfig(AppConfig):
-    name = 'tienda'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tienda' 
+
+    def ready(self):
+        # Conectamos la función al final de la migración
+        post_migrate.connect(create_default_config, sender=self)
